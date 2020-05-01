f63549d5a26191eca356f1856f0aba5ff086d066
Matt Warren <matthew.warren@contrastsecurity.com>
2020-05-01 16:19:25 +0100

    Add a test app (that can be debugged)

---
+++ TestApp-NetFramework/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TestApp_NetFramework
+{
+    class AnotherClassProgram { }
+    class YetAnotherClassProgram { }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Debugging test application 1");
+
+            Console.WriteLine("Running under .NET {0}", Environment.Version);
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.WriteLine("We are being debugged");
+            }
+            else
+            {
+                Console.WriteLine("We are not being debugged");
+            }
+
+            try
+            {
+                throw new InvalidOperationException("Testing");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            System.Diagnostics.Debug.WriteLine("Hello");
+            System.Diagnostics.Debugger.Break();
+            Console.WriteLine("Running again (Press <Enter> to exit)");
+
+            Console.ReadLine();
+            Console.WriteLine("Exiting");
+        }
+    }
+}
